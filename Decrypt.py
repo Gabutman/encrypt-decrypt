9e5caf6b8a653c049da1dc127ae108bfdc647044
Gabutman <90994222+Gabutman@users.noreply.github.com>
2021-09-19 09:25:09 +0700

    Decrypt.py
    Decrypt program

--- Decrypt.py
+++ Decrypt.py
@@ -11,6 +11,14 @@ def dm(t):
 def dm0(t):
 	return [ord(i) for i in t]
 t,d,s,m,k,md="","",list_text(1114112),dm(input("input the value:")),dm(input("input key:")),int(input("input mod:"))
+if m < 1:
+        m = 1
+if k < 1:
+        k = 1
+if md < 1:
+        md = 3
 for i in dm0(open(input("filename1:"),"r").read()):
 	t+=s[s.find(str(chr(int(i)-m)))-k%md]
-open("filename2:","w").write(t)
+f = open("filename2:","w")
+f.write(t)
+f.close()
